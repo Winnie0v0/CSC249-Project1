0a969dc9e51a94d02e1b6763544d4e88d64f8d41
Winnie0v0 <wzong@smith.edu>
2022-10-28 01:42:03 -0400

    Reformat for receiving request

--- server.py
+++ server.py
@@ -1,48 +1,50 @@
 import socket
 import threading
 
-IP = socket.gethostbyname(socket.gethostname())
-PORT = 5566
+IP = "0.0.0.0"
+PORT = 6789
 ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
-DISCONNECT_MSG = "!DISCONNECT"
+# DISCONNECT_MSG = "!DISCONNECT"
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
-
+    # TCP connection set up
     connected = True
     while connected:
         msg = conn.recv(SIZE).decode(FORMAT)
-        if msg == DISCONNECT_MSG:
-            connected = False
-
+        print("msg1111 "+msg)
+            # .decode(FORMAT)
+        filename = msg.split()[1]
+        f = open(filename[1:])
         print(f"[{addr}] {msg}")
-        # filename = msg
-        f = open(msg)
+
+        # f = open(filename)
         outputdata = f.read()
+        # Send the connection message
         conn.send(bytes("HTTP/1.1 200 OK","UTF-8"))
         conn.send(outputdata.encode())
-        # for i in range(0, len(outputdata)):
-        #     conn.send(outputdata[i].encode())
-        # conn.close()
-        # msg = f"Msg received: {msg}"
-        # conn.send(msg.encode(FORMAT))
 
-        conn.close()
+    conn.close()
+        # break
 
 def main():
     print("[STARTING] Server is starting...")
+    # Create a server socket
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to server address: IP and Port
     server.bind(ADDR)
+    # Ask the server to listen to multiple connections
     server.listen()
     print(f"[LISTENING] Server is listening on {IP}:{PORT}")
 
     while True:
         conn, addr = server.accept()
+        # Create a main thread
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 if __name__ == "__main__":
     main()
